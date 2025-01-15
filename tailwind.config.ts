97081862d0208331af69a139edaac6029af685b6
agha-naveed <naveedabs31@gmail.com>
2025-01-15 22:48:47 +0500

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,18 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        "opensans-light": ["opensans-light"],
+        "opensans-regular": ["opensans-regular"],
+        "opensans-medium": ["opensans-medium"],
+        "opensans-semibold": ["opensans-semibold"],
+        "opensans-bold": ["opensans-bold"],
+        "opensans-extraBold": ["opensans-extraBold"]
       },
+      colors: {
+        orangeClr: "#f08a06",
+        lightGray: "rgb(241, 241, 241)"
+      }
     },
   },
   plugins: [],
