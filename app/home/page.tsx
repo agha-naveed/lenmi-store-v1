ae8c284639d1cc2a4b8301f138514ed6d8963cb9
agha-naveed <naveedabs31@gmail.com>
2025-01-16 15:10:27 +0500

    trying carousel

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ImgSlider from '@/components/ImgSlider'
 import ProductCard from '../components/ProductCard'
+import Carousel from '../components/Carousel'
+
 
 export default function page() {
   return (
     <div className='w-full h-full'>
-      <ImgSlider />
+      <Carousel />
       <div className="container mx-auto">
         <div className="flex flex-wrap gap-5">
           <ProductCard />
