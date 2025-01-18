4a2644615b10d970bcdba1fa7861e6b8ae247919
agha-naveed <naveedabs31@gmail.com>
2025-01-18 18:28:07 +0500

    responsiveness

--- app/(home)/product/[id]/page.tsx
+++ app/(home)/product/[id]/page.tsx
@@ -35,9 +35,9 @@ export default function page() {
 
 
   return (
-    <section className='container mx-auto lg:grid lg:grid-cols-[auto_auto] grid-col-[auto] py-10 justify-between'>
+    <section className='container mx-auto grid xl:grid-cols-[auto_auto] grid-col-[auto] py-10 xl:justify-between justify-center justify-items-center gap-5'>
 
-      <div className='flex w-full gap-1'>
+      <div className='md:flex grid border xl:border-transparent rounded-xl border-black xl:w-full w-fit h-fit xl:gap-5 gap-3'>
         
         <div onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} className='w-[400px] h-[400px] p-3 overflow-hidden cursor-pointer border border-gray-400 rounded-xl'>
           <Image ref={imageRef} src="https://m.media-amazon.com/images/I/71zwwEe2nLL._AC_SL1500_.jpg" alt='Jacket' style={{transform: `scale(${zoom})`, transformOrigin: `${offset.x}px ${offset.y}px`, transition: 'tranform 0.2s ease-out'}} width={200} height={200} className='w-full h-full object-contain' />
@@ -98,7 +98,7 @@ export default function page() {
       </div>
 
 
-      <div className='font-opensans w-[370px] border border-black rounded-lg'>
+      <div className='font-opensans w-[370px] h-fit border border-black rounded-lg'>
         <div className='flex items-center justify-between  py-3 px-4'>
           <span className='font-semibold text-[15px]'>Ship to</span>
           <span className='flex gap-[2px] items-center font-medium text-[15px]'>
@@ -107,43 +107,49 @@ export default function page() {
           </span>
         </div>
 
-        <div className='w-full text-center p-2 h-14 bg-gradient-to-t from-transparent to-lightPeachClr'>
-          <span className='text-[15px] font-bold tracking-[-1px]'>Lenmi Store Commitment</span>
-        </div>
-
-        <div className='grid gap-1'>
-          <Link href={""} className='flex px-8 justify-between items-center py-3' title='Delivery Details'>
-            <div className='grid gap-1'>
-              <span className='font-bold text-[15px] text-black tracking-[-1px]'>
-                Free Shipping
-              </span>
-              <span className='text-gray-600'>
-                Delivery: <span className='font-bold text-black'>Aug 29 - Sep 04</span>
-              </span>
-            </div>
-            <span><IoIosArrowForward /></span>
-          </Link>
-
-          <div className='grid gap-[6px] px-8 py-3'>
-            <h5 className='text-black font-bold text-[16px] tracking-[-1px]'>Security & Privacy</h5>
-            <p className='text-gray-600 font-normal text-[12px] text-justify'>Safe payments: we do not share your personal details with any third parties without your consent. Secure personal details: We protect your privacy and keep your personal details safe and secure.</p>
-          </div>
-          <div className='px-4'>
-            <div className='w-full h-[1px] border-b-2 border-b-gray-300'></div>
-          </div>
-        </div>
-
-        <div className='px-4 py-3'>
-          <span className='font-bold'>Quantity</span>
+        <div className='md:flex'>
           <div>
-            <button className='px-1 py-[6px] w-9 text-[22px]' onClick={() => quantity > 1 ? setQuantity(quantity - 1) : null}>-</button>
-            <input className='p-1 font-medium text-center' type="number" placeholder='1' readOnly value={quantity} min={1} max={10} />
-            <button className='p-[6px] w-9 text-[22px]' onClick={() => setQuantity(quantity + 1)}>+</button>
+            <div className='w-full text-center p-2 h-14 bg-gradient-to-t from-transparent to-lightPeachClr'>
+              <span className='text-[15px] font-bold tracking-[-1px]'>Lenmi Store Commitment</span>
+            </div>
+
+            <div className='grid gap-1'>
+              <Link href={""} className='flex px-8 justify-between items-center py-3' title='Delivery Details'>
+                <div className='grid gap-1'>
+                  <span className='font-bold text-[15px] text-black tracking-[-1px]'>
+                    Free Shipping
+                  </span>
+                  <span className='text-gray-600'>
+                    Delivery: <span className='font-bold text-black'>Aug 29 - Sep 04</span>
+                  </span>
+                </div>
+                <span><IoIosArrowForward /></span>
+              </Link>
+
+              <div className='grid gap-[6px] px-8 py-3'>
+                <h5 className='text-black font-bold text-[16px] tracking-[-1px]'>Security & Privacy</h5>
+                <p className='text-gray-600 font-normal text-[12px] text-justify'>Safe payments: we do not share your personal details with any third parties without your consent. Secure personal details: We protect your privacy and keep your personal details safe and secure.</p>
+              </div>
+              <div className='px-4'>
+                <div className='w-full h-[1px] border-b-2 border-b-gray-300'></div>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className='grid p-4 gap-2'>
-          <button className='h-11 bg-slate-800 transition-all hover:bg-slate-900 text-white font-bold rounded-3xl'>Buy Now</button>
-          <button className='h-11 bg-slate-300 text-slate-800 transition-all hover:border hover:border-slate-800 hover:bg-transparent font-bold rounded-3xl'>Add to cart</button>
+
+          <div>
+            <div className='px-4 py-3'>
+              <span className='font-bold'>Quantity</span>
+              <div>
+                <button className='px-1 py-[6px] w-9 text-[22px]' onClick={() => quantity > 1 ? setQuantity(quantity - 1) : null}>-</button>
+                <input className='p-1 font-medium text-center' type="number" placeholder='1' readOnly value={quantity} min={1} max={10} />
+                <button className='p-[6px] w-9 text-[22px]' onClick={() => setQuantity(quantity + 1)}>+</button>
+              </div>
+            </div>
+            <div className='grid p-4 gap-2'>
+              <button className='h-11 bg-slate-800 transition-all hover:bg-slate-900 text-white font-bold rounded-3xl'>Buy Now</button>
+              <button className='h-11 bg-slate-300 text-slate-800 transition-all hover:border hover:border-slate-800 hover:bg-transparent font-bold rounded-3xl'>Add to cart</button>
+            </div>
+          </div>
         </div>
 
       </div>
