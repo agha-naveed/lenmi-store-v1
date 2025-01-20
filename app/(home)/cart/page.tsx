d9bc744b4c02ec155dbf5642c7e3bbc01bb6ead0
agha-naveed <naveedabs31@gmail.com>
2025-01-20 12:21:06 +0500

    mobile view done

--- app/(home)/cart/page.tsx
+++ app/(home)/cart/page.tsx
@@ -32,19 +32,19 @@ export default function page() {
                         <tbody>
                             <tr className='border-b-2'>
                                 <td>
-                                    <Link href={""} title='Lenovo | N21 | 6th Gen | 16GB Storage | 4GB RAM | 11.6″ Display | Rotatable Camera | Playstore Supported | Chromebook Daraz Like New Slightly Used Import American Stock' className='cursor-pointer flex gap-4 py-5 group'>
-                                        <span className='rounded-xl !h-fit p-1 !w-[220px] overflow-hidden'>
+                                    <Link href={""} title='Lenovo | N21 | 6th Gen | 16GB Storage | 4GB RAM | 11.6″ Display | Rotatable Camera | Playstore Supported | Chromebook Daraz Like New Slightly Used Import American Stock' className='cursor-pointer flex sm:gap-4 gap-2 py-5 group'>
+                                        <span className='rounded-xl !h-fit p-1 sm:!w-[220px] w-[190px] overflow-hidden'>
                                             <Image src="https://typeshop.pk/wp-content/uploads/2024/09/749789-product-0-i-638518506623874710_06458751-c09a-4dec-b82f-416beff7e94f.webp" className='w-full h-full object-cover' alt='pikachu' width={100} height={100} />
                                         </span>
                                         <span className='w-full'>
-                                            <h2 className='font-semibold tracking-[-1px] md:w-[60%] w-full line-clamp-2 my-2 transition-all group-hover:text-orangeClr'>Lenovo | N21 | 6th Gen | 16GB Storage | 4GB RAM | 11.6″ Display | Rotatable Camera | Playstore Supported | Chromebook Daraz Like New Slightly Used Import American Stock</h2>
+                                            <h2 className='font-semibold tracking-[-1px] md:w-[60%] w-full line-clamp-2 my-2 sm:text-[16px] text-[15px] transition-all group-hover:text-orangeClr'>Lenovo | N21 | 6th Gen | 16GB Storage | 4GB RAM | 11.6″ Display | Rotatable Camera | Playstore Supported | Chromebook Daraz Like New Slightly Used Import American Stock</h2>
 
 
-                                            <p className='text-[15px] mt-[15px]'>Color: <span className='font-semibold'>Black</span></p>
-                                            <p className='text-[15px]'>Category: <span className='font-semibold'>Electonics</span></p>
+                                            <p className='sm:text-[15px] text-[13px] mt-[10px]'>Color: <span className='font-semibold'>Black</span></p>
+                                            <p className='sm:text-[15px] text-[13px]'>Category: <span className='font-semibold'>Electonics</span></p>
 
 
-                                            <span className='text-[13px] flex items-center gap-2 py-3'>
+                                            <span className='text-[13px] items-center gap-2 py-3 md:flex hidden'>
                                                 <div className='flex'>
                                                     <GoStarFill />
                                                     <GoStarFill />
@@ -53,6 +53,13 @@ export default function page() {
                                                     <GoStarFill />
                                                 </div>
                                                 <p className='text-gray-600  content-center'>10000+ sold</p>
+                                            </span>
+
+                                            <span className='md:hidden block'>
+                                                <h5 className='font-medium'>
+                                                    <span className='text-[14px]'> Price: </span>
+                                                    <span className='font-bold text-orangeClr'>$99.00</span>
+                                                </h5>
                                             </span>
                                         </span>
                                     </Link>
