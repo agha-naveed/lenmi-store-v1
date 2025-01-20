6ea3dec02cfe677a06d3f21d530f1c2700dc62d5
agha-naveed <naveedabs31@gmail.com>
2025-01-20 15:08:24 +0500

    adding categories

--- app/components/CategoriesDesign.tsx
+++ app/components/CategoriesDesign.tsx
@@ -11,28 +11,49 @@ export default function CategoriesDesign() {
             <div className='w-full h-auto bg-orangeClr'></div>
 
             <div className='w-fit h-full text-white flex flex-wrap justify-end gap-[11px]'>
-                <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-slate-600 hover:scale-105 text-black hover:text-white hover:shadow-custom-light'>
+            <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
                     <span className='flex text-[19px] font-semibold pt-5 pl-5'>Home & Garden</span>
-                    <div className='content-center'>
-                        <Image src={plantRender} alt='Plant render' className='w-auto !h-[130px]' />
+                    <div className='content-center relative'>
+                        <Image src={plantRender} alt='Plant render' className='relative z-10 w-auto !h-[130px]' />
+                        <Image src={plantRender} alt='Plant render' className='absolute left-32 transition_2 group-hover:left-10 blur-[2px] z-[0] -top-2 w-[450px] h-fit opacity-50' />
                     </div>
                 </div>
-                <div className='bg-gray-700 w-72 h-36'>
-                    <span>Home & Garden</span>
+                <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
+                    <span className='flex text-[19px] font-semibold pt-5 pl-5'>Home & Garden</span>
+                    <div className='content-center relative'>
+                        <Image src={plantRender} alt='Plant render' className='relative z-10 w-auto !h-[130px]' />
+                        <Image src={plantRender} alt='Plant render' className='absolute left-32 transition_2 group-hover:left-10 blur-[2px] z-[0] -top-2 w-[450px] h-fit opacity-50' />
+                    </div>
                 </div>
-                <div className='bg-gray-700 w-72 h-36'>
-                    <span>Home & Garden</span>
+                <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
+                    <span className='flex text-[19px] font-semibold pt-5 pl-5'>Home & Garden</span>
+                    <div className='content-center relative'>
+                        <Image src={plantRender} alt='Plant render' className='relative z-10 w-auto !h-[130px]' />
+                        <Image src={plantRender} alt='Plant render' className='absolute left-32 transition_2 group-hover:left-10 blur-[2px] z-[0] -top-2 w-[450px] h-fit opacity-50' />
+                    </div>
                 </div>
-                <div className='bg-gray-700 w-72 h-36'>
-                    <span>Home & Garden</span>
+                <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
+                    <span className='flex text-[19px] font-semibold pt-5 pl-5'>Home & Garden</span>
+                    <div className='content-center relative'>
+                        <Image src={plantRender} alt='Plant render' className='relative z-10 w-auto !h-[130px]' />
+                        <Image src={plantRender} alt='Plant render' className='absolute left-32 transition_2 group-hover:left-10 blur-[2px] z-[0] -top-2 w-[450px] h-fit opacity-50' />
+                    </div>
                 </div>
-                <div className='bg-gray-700 w-72 h-36'>
-                    <span>Home & Garden</span>
+                <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
+                    <span className='flex text-[19px] font-semibold pt-5 pl-5'>Home & Garden</span>
+                    <div className='content-center relative'>
+                        <Image src={plantRender} alt='Plant render' className='relative z-10 w-auto !h-[130px]' />
+                        <Image src={plantRender} alt='Plant render' className='absolute left-32 transition_2 group-hover:left-10 blur-[2px] z-[0] -top-2 w-[450px] h-fit opacity-50' />
+                    </div>
                 </div>
-                <div className='bg-gray-700 w-72 h-36'>
-                    <span>Home & Garden</span>
-                    
+                <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
+                    <span className='flex text-[19px] font-semibold pt-5 pl-5'>Home & Garden</span>
+                    <div className='content-center relative'>
+                        <Image src={plantRender} alt='Plant render' className='relative z-10 w-auto !h-[130px]' />
+                        <Image src={plantRender} alt='Plant render' className='absolute left-32 transition_2 group-hover:left-10 blur-[2px] z-[0] -top-2 w-[450px] h-fit opacity-50' />
+                    </div>
                 </div>
+                
             </div>
         </div>
   )
