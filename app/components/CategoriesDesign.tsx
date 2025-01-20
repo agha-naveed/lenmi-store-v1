70a90459661fe28884a8f06dff15fa4accb7e3c8
agha-naveed <naveedabs31@gmail.com>
2025-01-20 15:39:38 +0500

    .

--- app/components/CategoriesDesign.tsx
+++ app/components/CategoriesDesign.tsx
@@ -3,56 +3,60 @@
 import React from 'react'
 import plantRender from '@/images/plant_render.png'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function CategoriesDesign() {
   return (
         <div className='flex container mx-auto py-10 font-opensans'>
             
-            <div className='w-full h-auto bg-orangeClr'></div>
+            <div className='w-full h-auto flex justify-center items-center bg-slate-800'>
+                <span className='font-semibold text-3xl text-white'>Categories</span>
+            </div>
 
             <div className='w-fit h-full text-white flex flex-wrap justify-end gap-[11px]'>
-            <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
+                <Link href={""} className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
                     <span className='flex text-[19px] font-semibold pt-5 pl-5'>Home & Garden</span>
                     <div className='content-center relative'>
                         <Image src={plantRender} alt='Plant render' className='relative z-10 w-auto !h-[130px]' />
                         <Image src={plantRender} alt='Plant render' className='absolute left-32 transition_2 group-hover:left-10 blur-[2px] z-[0] -top-2 w-[450px] h-fit opacity-50' />
                     </div>
-                </div>
-                <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
+                </Link>
+                <Link href={""} className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
                     <span className='flex text-[19px] font-semibold pt-5 pl-5'>Home & Garden</span>
                     <div className='content-center relative'>
                         <Image src={plantRender} alt='Plant render' className='relative z-10 w-auto !h-[130px]' />
                         <Image src={plantRender} alt='Plant render' className='absolute left-32 transition_2 group-hover:left-10 blur-[2px] z-[0] -top-2 w-[450px] h-fit opacity-50' />
                     </div>
-                </div>
-                <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
+                </Link>
+                <Link href={""} className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
                     <span className='flex text-[19px] font-semibold pt-5 pl-5'>Home & Garden</span>
                     <div className='content-center relative'>
                         <Image src={plantRender} alt='Plant render' className='relative z-10 w-auto !h-[130px]' />
                         <Image src={plantRender} alt='Plant render' className='absolute left-32 transition_2 group-hover:left-10 blur-[2px] z-[0] -top-2 w-[450px] h-fit opacity-50' />
                     </div>
-                </div>
-                <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
+                </Link>
+                <Link href={""} className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
                     <span className='flex text-[19px] font-semibold pt-5 pl-5'>Home & Garden</span>
                     <div className='content-center relative'>
                         <Image src={plantRender} alt='Plant render' className='relative z-10 w-auto !h-[130px]' />
                         <Image src={plantRender} alt='Plant render' className='absolute left-32 transition_2 group-hover:left-10 blur-[2px] z-[0] -top-2 w-[450px] h-fit opacity-50' />
                     </div>
-                </div>
-                <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
+                </Link>
+                <Link href={""} className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
                     <span className='flex text-[19px] font-semibold pt-5 pl-5'>Home & Garden</span>
                     <div className='content-center relative'>
                         <Image src={plantRender} alt='Plant render' className='relative z-10 w-auto !h-[130px]' />
                         <Image src={plantRender} alt='Plant render' className='absolute left-32 transition_2 group-hover:left-10 blur-[2px] z-[0] -top-2 w-[450px] h-fit opacity-50' />
                     </div>
-                </div>
-                <div className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
+                </Link>
+                <Link href={""} className='bg-zinc-200 w-72 h-36 flex overflow-hidden transition-all cursor-pointer hover:bg-gray-300 hover:scale-105 relative text-black hover:shadow-custom-light group'>
                     <span className='flex text-[19px] font-semibold pt-5 pl-5'>Home & Garden</span>
                     <div className='content-center relative'>
                         <Image src={plantRender} alt='Plant render' className='relative z-10 w-auto !h-[130px]' />
                         <Image src={plantRender} alt='Plant render' className='absolute left-32 transition_2 group-hover:left-10 blur-[2px] z-[0] -top-2 w-[450px] h-fit opacity-50' />
                     </div>
-                </div>
+                </Link>
+                
                 
             </div>
         </div>
