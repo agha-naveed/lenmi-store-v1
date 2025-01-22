8df1a31b541ba483168295d718e1a4cd19eeae76
agha-naveed <naveedabs31@gmail.com>
2025-01-22 17:26:30 +0500

    dark loader effect

--- app/components/Loading.tsx
+++ app/components/Loading.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function MyLoader() {
   return (
-    <div className='w-full h-screen flex flex-col justify-center items-center'>
+    <div className='w-full h-screen flex flex-col justify-center items-center bg-slate-800'>
         
       <div className='flex items-center'>
         <div className="pacman"></div>
@@ -13,7 +13,7 @@ export default function MyLoader() {
           <div className="dot"></div>
         </div>
       </div>
-      <p className='font-opensans font-semibold mt-2 text-[22px] tracking-[-1px]'>Loading...</p>
+      <p className='font-opensans font-semibold mt-2 text-[22px] text-white tracking-[-1px]'>Loading...</p>
 
     </div>
   )
