9f2484d86b3f74499f6eaf9336158ea66a6086f5
agha-naveed <naveedabs31@gmail.com>
2025-01-28 19:31:29 +0500

    add product page

--- app/(home)/add-product/page.tsx
+++ app/(home)/add-product/page.tsx
@@ -1,10 +1,15 @@
 'use client'
 import React from 'react'
+import { MdAddPhotoAlternate } from "react-icons/md";
 
 export default function page() {
-  return (
-    <div className='container mx-auto'>
-        
-    </div>
-  )
+    return (
+        <div className='container mx-auto'>
+            <div>
+                <div className='border border-dashed border-gray-500 rounded-lg w-fit p-10'>
+                    <MdAddPhotoAlternate className='text-4xl text-gray-500' />
+                </div>
+            </div>
+        </div>
+    )
 }
