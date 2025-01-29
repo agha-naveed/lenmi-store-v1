cecc04a30495dc2861e0ed1cf767add7bd02316e
agha-naveed <naveedabs31@gmail.com>
2025-01-29 19:10:22 +0500

    .

--- app/(home)/add-product/page.tsx
+++ app/(home)/add-product/page.tsx
@@ -5,6 +5,7 @@ import { MdAddPhotoAlternate } from "react-icons/md";
 import img from '@/images/jethalal.jpeg'
 import { useForm } from 'react-hook-form';
 import Form from 'next/form';
+import { IoCloseCircle } from "react-icons/io5";
 
 export default function page() {
 
@@ -17,7 +18,7 @@ export default function page() {
         account_type: string
     }
 
-    let [selectedImage, setSelectedImage] = useState<string | undefined>()
+    let [selectedImage, setSelectedImage] = useState<string[]>([])
      
     const { register, handleSubmit, formState: { errors } } = useForm<IFormInputs>();
 
@@ -25,31 +26,65 @@ export default function page() {
     return (
         <div className='container mx-auto'>
             <Form action={""} formMethod='POST'>
-                <div className='flex gap-3'>
+                <div className='flex gap-3 shadow-lg shadow-gray-400 rounded-lg w-fit p-5'>
                     
                     <div title='Upload Picture'>
                         <label htmlFor="upload-product-pic" className='cursor-pointer flex justify-center items-center w-[120px] h-[120px] border border-dashed border-gray-500 rounded-lg'>
                             <MdAddPhotoAlternate className='text-4xl text-gray-500' />
                         </label>
                         <input type="file" accept='image/*'
-                         id='upload-product-pic'
+                         id='upload-product-pic' disabled={selectedImage.length == 3 ? true : false}
                          className='hidden'
-                         onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                            // const file = e.target?.files?.[0];
-                            // setSelectedImage(file ? URL.createObjectURL(file) : "@/images/jethalal.jpeg")
+                         onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                            const file = event.target.files?.[0];
+                            if(file) {
+                                setSelectedImage((prevImages) => [...prevImages, URL.createObjectURL(file)]);
+                            }
                          }}
                          />
                     </div>
                     <div className='flex justify-center items-center w-[120px] h-[120px] border border-gray-500 rounded-lg overflow-hidden'>
-                        <Image src={img} alt='' className='w-full h-full object-cover' />
+                    {
+                        selectedImage[0] ? 
+                        <>
+                            <Image src={selectedImage[0]} alt='' width={300} height={300} className='w-full h-full object-cover' />
+                            <IoCloseCircle className='' />
+                        </>
+                        : null
+                    }
                     </div>
                     <div className='flex justify-center items-center w-[120px] h-[120px] border border-gray-500 rounded-lg overflow-hidden'>
-                        <Image src={img} alt='' className='w-full h-full object-cover' />
+                    {
+                        selectedImage[1] ? 
+                        <>
+                            <Image src={selectedImage[1]} alt='' width={300} height={300} className='w-full h-full object-cover' />
+                        </>
+                        : null
+                    }
                     </div>
                     <div className='flex justify-center items-center w-[120px] h-[120px] border border-gray-500 rounded-lg'>
-                        <span className='select-none text-gray-500 font-opensans font-medium'>Photo</span>
+                    {
+                        selectedImage[2] ? 
+                        <Image src={selectedImage[2]} alt='' width={300} height={300} className='w-full h-full object-cover' />
+                        : null
+                    }
                     </div>
-                    
+                </div>
+
+                <div className='font-opensans'>
+                    <div className='flex'>
+                        <div className='grid'>
+                            <label htmlFor="" className='text-[15px] font-medium'>Product Name</label>
+                            <input type="text" className='border' />
+                        </div>
+                        <div className='grid'>
+                            <label htmlFor="" className='text-[15px] font-medium'>Category</label>
+                            <select name="" id="">
+                                <option value="">-- select --</option>
+                                <option value={`electronics`}>Electronics</option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
             </Form>
         </div>
