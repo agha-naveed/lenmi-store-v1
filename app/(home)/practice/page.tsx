22dc5ec48f457e59aa967db1624d3f58c3666902
agha-naveed <naveedabs31@gmail.com>
2025-02-03 19:40:36 +0500

    using cloudinary

--- app/(home)/practice/page.tsx
+++ app/(home)/practice/page.tsx
@@ -1,25 +1,15 @@
 'use client'
-import React from 'react'
-import { getDatabase, ref, set } from 'firebase/database'
-import { app } from '@/lib/firebase/firebase'
 
-const db = getDatabase(app)
+import React from 'react'
 
 export default function page() {
 
-    const putData = () => {
-        set(ref(db, 'users/myData'), {
-            id: 1,
-            name: "naveed",
-            age: 21
-        })
-    }
 
     return (
         <div>
             <label htmlFor="">Hello from Naveed</label>
             <br />
-            <button onClick={() => putData()} className='bg-slate-800 text-white px-3 py-2'>Click Me!</button>
+            <button className='bg-slate-800 text-white px-3 py-2'>Click Me!</button>
         </div>
     )
 }
