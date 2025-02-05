330df36473eab92726aa02b545f45c044cc6fc9e
agha-naveed <naveedabs31@gmail.com>
2025-02-05 11:06:11 +0500

    get handler done for product using id and title

--- app/(home)/product/[id]/api/route.ts
+++ app/(home)/product/[id]/api/route.ts
@@ -1,11 +1,20 @@
+import dbConnection from "@/lib/database/dbConnection";
+import Product from "@/lib/database/model/product";
 // import 
 
-import { NextApiRequest } from "next";
 import { NextRequest, NextResponse } from "next/server";
 
 
 export async function GET(req:NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    await dbConnection()
+
     let p_id = await params
 
-    return NextResponse.json({ms:"as"})
+    let data = await Product.findOne({_id: p_id})
+
+
+    return NextResponse.json({
+        message: "ok",
+        data
+    }, { status: 200 })
 }
