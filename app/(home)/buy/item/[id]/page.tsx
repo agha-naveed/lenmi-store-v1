199b7be0cf991878c66e5546632cf22fc5c68797
agha-naveed <naveedabs31@gmail.com>
2025-02-08 17:35:07 +0500

    get handler done

--- app/(home)/buy/item/[id]/page.tsx
+++ app/(home)/buy/item/[id]/page.tsx
@@ -17,7 +17,6 @@ export default function page() {
 
       if (res.status == 200) {
         setProductDetails(await res.data.data);
-        console.log(await res.data.data)
       }
     };
     fetchData();
@@ -30,7 +29,7 @@ export default function page() {
           <table className="w-full">
             <thead>
               <tr className="font-semibold">
-                <th className="text-start">Products</th>
+                <th className="text-start">Product</th>
                 <th className="text-center md:table-cell hidden">Price</th>
                 <th className="text-center md:table-cell hidden">Quantity</th>
                 <th className="text-center md:table-cell hidden">
@@ -40,77 +39,87 @@ export default function page() {
             </thead>
 
             <tbody>
-              {
-                <tr className={`relative`}>
-                  <td>
-                    <Link
-                      href={""}
-                      title={productDetails.productData.name}
-                      className="cursor-pointer flex items-center sm:gap-4 gap-2 py-5 group"
-                    >
-                      <span className="flex rounded-xl !h-fit p-1 sm:!w-[220px] w-[190px] overflow-hidden">
-                        <Image
-                          src={productDetails.productData.imgURL[0]}
-                          className="w-full h-full object-cover"
-                          alt="pikachu"
-                          width={100}
-                          height={100}
-                        />
-                      </span>
-                      <span className="w-full">
-                        <h2 className="font-semibold tracking-[-1px] md:w-[60%] w-full line-clamp-2 my-2 sm:text-[16px] text-[15px] transition-all group-hover:text-orangeClr">
-                          {productDetails.productData.name}
-                        </h2>
+                {
+                    productDetails ? 
+                    <tr className={`relative`}>
+                        <td>
+                            <Link
+                            href={""}
+                            title={productDetails.productData.name}
+                            className="cursor-pointer flex items-center sm:gap-4 gap-2 py-5 group"
+                            >
+                                <span className="flex rounded-xl !h-fit p-1 sm:!w-[220px] w-[190px] overflow-hidden">
+                                    <Image
+                                    src={productDetails.productData.imgURL[0]}
+                                    className="w-full h-full object-cover"
+                                    alt="pikachu"
+                                    width={100}
+                                    height={100}
+                                    />
+                                </span>
+                                <span className="w-full">
+                                    <h2 className="font-semibold tracking-[-1px] md:w-[60%] w-full line-clamp-2 my-2 sm:text-[16px] text-[15px] transition-all group-hover:text-orangeClr">
+                                    {productDetails.productData.name}
+                                    </h2>
 
-                        <p className="sm:text-[15px] text-[13px] mt-[10px]">
-                          Color: <span className="font-semibold">Black</span>
-                        </p>
-                        <p className="sm:text-[15px] text-[13px]">
-                          Category:{" "}
-                          <span className="font-semibold">
-                            {productDetails.productData.category}
-                          </span>
-                        </p>
+                                    <p className="sm:text-[15px] text-[13px] mt-[10px]">
+                                    Color: <span className="font-semibold">Black</span>
+                                    </p>
+                                    <p className="sm:text-[15px] text-[13px]">
+                                    Category:{" "}
+                                    <span className="font-semibold">
+                                        {productDetails.productData.category}
+                                    </span>
+                                    </p>
 
-                        <span className="text-[13px] items-center gap-2 py-3 md:flex hidden">
-                          <div className="flex">
-                            <GoStarFill />
-                            <GoStarFill />
-                            <GoStarFill />
-                            <GoStarFill />
-                            <GoStarFill />
-                          </div>
-                          <p className="text-gray-600  content-center">
-                            10000+ sold
-                          </p>
-                        </span>
+                                    <span className="text-[13px] items-center gap-2 py-3 md:flex hidden">
+                                    <div className="flex">
+                                        <GoStarFill />
+                                        <GoStarFill />
+                                        <GoStarFill />
+                                        <GoStarFill />
+                                        <GoStarFill />
+                                    </div>
+                                    <p className="text-gray-600  content-center">
+                                        10000+ sold
+                                    </p>
+                                    </span>
 
-                        <span className="md:hidden block mt-1">
-                          <h5 className="font-medium">
-                            <span className="text-[15px]"> Price: </span>
-                            <span className="font-bold text-[17px] text-orangeClr">
-                              PKR {productDetails.productData.price}
+                                    <span className="md:hidden block mt-1">
+                                    <h5 className="font-medium">
+                                        <span className="text-[15px]"> Price: </span>
+                                        <span className="font-bold text-[17px] text-orangeClr">
+                                        PKR {productDetails.productData.price}
+                                        </span>
+                                    </h5>
+                                    </span>
+                                </span>
+                            </Link>
+                        </td>
+
+                        <td className="p-5 text-center md:table-cell hidden">
+                            <span className="content-center">
+                            <h3 className="font-bold">{productDetails.productData.price}</h3>
                             </span>
-                          </h5>
-                        </span>
-                      </span>
-                    </Link>
-                  </td>
+                        </td>
 
-                  <td className="p-5 text-center md:table-cell hidden">
-                    <span className="content-center">
-                      <h3 className="font-bold">{productDetails.productData.price}</h3>
-                    </span>
-                  </td>
-
-                  <td className="p-5 text-center md:table-cell hidden">
-                    <span className="content-center">
-                      <h3 className="font-bold">{productDetails.productData.price}</h3>
-                    </span>
-                  </td>
-                </tr>
-              }
+                        <td className="p-5 text-center md:table-cell hidden">
+                            <span className="content-center">
+                            <h3 className="font-bold">{productDetails.itemQuantity}</h3>
+                            </span>
+                        </td>
+                        
+                        <td className="p-5 text-center md:table-cell hidden">
+                            <span className="content-center">
+                            <h3 className="font-bold">{(productDetails.itemQuantity) * (productDetails.productData.price)}</h3>
+                            </span>
+                        </td>
+                    </tr>
+                    :
+                    <tr><td>Loading...</td></tr>
+                }
             </tbody>
+
           </table>
         </div>
       </div>
