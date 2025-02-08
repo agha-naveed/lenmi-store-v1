8d841ed06950a419e5dd7e68eac52c9a8c8d04c8
agha-naveed <naveedabs31@gmail.com>
2025-02-08 18:53:13 +0500

    .

--- app/(home)/buy/item/[id]/page.tsx
+++ app/(home)/buy/item/[id]/page.tsx
@@ -26,7 +26,7 @@ export default function page() {
   }, []);
 
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto font-opensans">
         
       <div className="w-full h-full rounded-2xl shadow-xl bg-white">
         <div className="md:p-7 p-2">
@@ -129,22 +129,41 @@ export default function page() {
       </div>
 
       <section>
-            <div>
-                <div>
+            <div className="w-full h-full mt-5 rounded-2xl shadow-xl bg-white md:p-7 p-2">
+                <h4 className="mb-5 text-[18px] font-semibold">Delivery Details</h4>
+                <div className="flex flex-wrap gap-3">
+                  <div className="grid gap-1">
+                      <label htmlFor="">Recipient's Name</label>
+                      <input type="text" className="border h-9 px-3 rounded-lg" />
+                  </div>
+
+                  <div className="grid gap-1">
+                      <label htmlFor="">Phone Number</label>
+                      <input type="number" min={11} className="border h-9 px-3 rounded-lg" />
+                  </div>
+
+                  <div className="grid w-44">
                     <label htmlFor="">District</label>
-                    <select name="" id="">
+                    <select name="" id="" className="h-9 p-2">
                         <option value="">-- select --</option>
-                        <option value="khaplu">Ghanche</option>
-                        <option value="khaplu">Rondu</option>
-                        <option value="khaplu">Shigar</option>
-                        <option value="khaplu">Kharmang</option>
-                        <option value="khaplu">Gilgit</option>
-                        <option value="khaplu">Astore</option>
-                        <option value="khaplu">Ghizer</option>
-                        <option value="khaplu">Nagar</option>
-                        <option value="khaplu">Hunza</option>
+                        <option value="Ghanche">Ghanche</option>
+                        <option value="Rondu">Rondu</option>
+                        <option value="Shigar">Shigar</option>
+                        <option value="Kharmang">Kharmang</option>
+                        <option value="Gilgit">Gilgit</option>
+                        <option value="Astore">Astore</option>
+                        <option value="Ghizer">Ghizer</option>
+                        <option value="Nagar">Nagar</option>
+                        <option value="Hunza">Hunza</option>
                     </select>
+                  </div>    
                 </div>
+
+                <div>
+                  <label htmlFor="">Full Address</label>
+                  <textarea name="" id=""></textarea>
+                </div>
+
             </div>
         </section>
 
