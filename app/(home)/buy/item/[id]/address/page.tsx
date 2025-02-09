156a9d2da501edaa3dc54f55ccd483a559921d3f
agha-naveed <naveedabs31@gmail.com>
2025-02-09 17:50:04 +0500

    .

--- app/(home)/buy/item/[id]/address/page.tsx
+++ app/(home)/buy/item/[id]/address/page.tsx
@@ -1,6 +1,9 @@
 'use client'
 import React, { useEffect } from 'react'
 import { useBuyContext } from "@/app/components/BuyContext";
+import Image from 'next/image';
+import cod from '@/images/payment-methods/cod.png'
+
 export default function page() {
 
   const { buyData, setBuyData }:any = useBuyContext();
@@ -13,8 +16,9 @@ export default function page() {
       <div className='rounded-2xl shadow-xl bg-white font-opensans py-3 px-6'>
         <h1 className='text-[18px] font-semibold'>Payment Method:</h1>
 
-        <div>
-          <div>
+        <div className='flex'>
+          <div className='w-32 h-32 bg-zinc-300 rounded-lg flex flex-col justify-center items-center'>
+            <Image src={cod} alt='Cash on Delivery' className='w-16' />
             <span>Cash on Delivery</span>
           </div>
           <div>
