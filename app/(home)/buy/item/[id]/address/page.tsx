aeacb3a3462e36beb30577a7759399b3a8277e05
agha-naveed <naveedabs31@gmail.com>
2025-02-09 15:15:06 +0500

    .

--- app/(home)/buy/item/[id]/address/page.tsx
+++ app/(home)/buy/item/[id]/address/page.tsx
@@ -4,10 +4,27 @@ import { useBuyContext } from "@/app/components/BuyContext";
 export default function page() {
 
   const { buyData, setBuyData }:any = useBuyContext();
-  
+
+
   
   
   return (
-    <div>page</div>
+    <div className='container mx-auto py-8'>
+      <div className='rounded-2xl shadow-xl bg-white font-opensans py-3 px-6'>
+        <h1 className='text-[18px] font-semibold'>Payment Method:</h1>
+
+        <div>
+          <div>
+            <span>Cash on Delivery</span>
+          </div>
+          <div>
+            <span>Credit Card</span>
+          </div>
+          <div>
+            <span>Online Banking</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
