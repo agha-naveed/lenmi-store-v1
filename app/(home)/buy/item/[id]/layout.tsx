758377f546f3bd599e702080232c8ebaa0cd482f
agha-naveed <naveedabs31@gmail.com>
2025-02-09 11:51:07 +0500

    .

--- app/(home)/buy/item/[id]/layout.tsx
+++ app/(home)/buy/item/[id]/layout.tsx
@@ -1,11 +1,14 @@
 "use client";
 import React from "react";
+import { BuyDataProvider } from '@/components/BuyContext'
 
 export default function layout({ children } : {children: React.ReactNode}) {
 
   return (
     <div>
+      <BuyDataProvider>
         {children}
+      </BuyDataProvider>
     </div>
   )
 }
--- app/components/BuyContext.tsx
+++ app/components/BuyContext.tsx
@@ -3,27 +3,42 @@
 import "../globals.css";
 import { createContext, useContext, useState } from "react";
 
+type DeliveryAddress = {
+  recipientName: string;
+  phone_number: number;
+  district: string;
+  address: string;
+};
 
 type BuyData = {
   userId: number;
   productId: number;
   quantity: number;
-  deliveryAddress: object;
+  deliveryAddress: DeliveryAddress;
 }
 
 const defaultBuyData: BuyData = {
   userId: 0,
   productId: 0,
   quantity: 0,
-  deliveryAddress: {},
+  deliveryAddress: {
+    recipientName: "",
+    phone_number: 0,
+    district: "",
+    address: ""
+  },
 }
 
-const buyContext = createContext<BuyData>(defaultBuyData)
+const buyContext = createContext<{
+  buyData: BuyData;
+  setBuyData: React.Dispatch<React.SetStateAction<BuyData>>;
+} | undefined>(undefined);
 
-export function buyDataProvider({ children }: { children: React.ReactNode }) {
 
+export function BuyDataProvider({ children }: { children: React.ReactNode }) {
+  const [buyData, setBuyData] = useState<BuyData>(defaultBuyData);
   return (
-      <buyContext.Provider value={defaultBuyData}>
+      <buyContext.Provider value={{buyData, setBuyData}}>
         {children}
       </buyContext.Provider>
   );
