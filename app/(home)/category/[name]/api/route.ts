5b0ea1f19c564ba2b4a641edfbb8d2738a3f94c8
agha-naveed <naveedabs31@gmail.com>
2025-02-18 10:45:03 +0500

    .

--- app/(home)/category/[name]/api/route.ts
+++ app/(home)/category/[name]/api/route.ts
@@ -1,6 +1,10 @@
 import dbConnection from "@/lib/database/dbConnection";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req:NextRequest, { params }: { params: Promise<{ id: string }> }) {
     await dbConnection()
+
+    return NextResponse.json({
+        message: "ok"
+    })
 }
--- app/(home)/category/[name]/page.tsx
+++ app/(home)/category/[name]/page.tsx
@@ -1,21 +1,27 @@
 'use client'
 import axios from 'axios'
 import { useParams } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function page() {
   const param = useParams()
+  const [products, setProducts] = useState([])
+
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get(`/category/${param.name}/api`)
-      
-    }
-    fetchData()
+    // const fetchData = async () => {
+    //   const res = await axios.get(`/category/${param.name}/api`)
+    //   if(res.status == 200) {
+    //     setProducts(await res.data.data)
+    //   }
+    // }
+    // fetchData()
   }, [])
 
   return (
-    <div>
-      
-    </div>
+    <section>
+      <div className='category-bg h-48 bg-slate-800 text-white'>
+        <h3>Electronics</h3>
+      </div>
+    </section>
   )
 }
