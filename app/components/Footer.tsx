1b7359b2969dfa886d91dd80d3becf0e5429938c
agha-naveed <naveedabs31@gmail.com>
2025-02-18 10:05:47 +0500

    footer responsiveness

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -15,13 +15,13 @@ import appstore from '@/images/app-downloads/appstore.png'
 export default function Footer() {
     return (
         <footer className="bg-slate-800 font-opensans mt-10">
-            <div className="container mx-auto py-10 flex gap-16">
-                <div className="flex flex-col gap-10">
+            <div className="container mx-auto py-10 flex md:flex-row flex-col gap-16">
+                <div className="flex flex-col md:gap-10 gap-4 items-center">
                     <Link href={"/"} className="w-36">
                         <Image src={logo} alt="Lenmi Store Logo" width={300} height={300} className="w-full h-full" />
                     </Link>
 
-                    <div className="flex text-[22px] justify-evenly text-white">
+                    <div className="flex text-[22px] md:justify-evenly gap-4 text-white">
                         <Link href={'https://facebook.com/naveedbalti31'} target="_blank">
                             <FaFacebook />
                         </Link>
@@ -38,9 +38,9 @@ export default function Footer() {
 
                 </div>
 
-                <div className="flex flex-wrap gap-10">
-                    <div className="flex gap-8">
-                        <div className="text-white flex flex-col text-[14px] gap-1">
+                <div className="flex flex-wrap justify-center gap-10">
+                    <div className="flex md:flex-row flex-col gap-8">
+                        <div className="text-white flex flex-col text-[14px] gap-1 md:items-start items-center">
                             <h4 className="font-bold text-[16px] pb-1">Customer Service</h4>
                             <Link className="w-fit" href={""}>Help Center</Link>
                             <Link className="w-fit" href={""}>Transaction Services</Link>
@@ -48,7 +48,7 @@ export default function Footer() {
                             <Link className="w-fit" href={""}>Returns & Refunds</Link>
                         </div>
                         
-                        <div className="text-white flex flex-col text-[14px] gap-1">
+                        <div className="text-white flex flex-col text-[14px] gap-1 md:items-start items-center">
                             <h4 className="font-bold text-[16px] pb-1">Shopping with us</h4>
                             <Link className="w-fit" href={""}>Making Payments</Link>
                             <Link className="w-fit" href={""}>Delivery Options</Link>
@@ -56,21 +56,23 @@ export default function Footer() {
                         </div>
                     </div>
 
-                    <div className="text-white flex flex-col text-[14px] gap-1">
-                        <h4 className="font-bold text-[16px] pb-1">Payment Methods</h4>
-                        <div className="flex items-center gap-1">
-                            <Image className="h-10 w-fit bg-white rounded-md p-1" title="Cash on Delivery" src={cod} alt="Cash on Delivery" />
-                            <Image className="h-10 w-fit bg-white rounded-md p-1" src={easypaisa} alt="Easypaisa" title="Easypaisa" />
-                            <Image className="h-10 w-fit bg-white rounded-md p-1" src={jazzcash} alt="Jazzcash" title="JazzCash" />
-                            <Image className="h-10 w-fit bg-white rounded-md p-1" src={card} alt="Debit/Credit Card" title="Debit/Credit Card" />
-                        </div>
+                    <div className="flex md:flex-row flex-col gap-10 items-center md:text-start text-center">
+                        <div className="text-white flex flex-col text-[14px] gap-1">
+                            <h4 className="font-bold text-[16px] pb-1">Payment Methods</h4>
+                            <div className="flex items-center gap-1">
+                                <Image className="h-10 w-fit bg-white rounded-md p-1" title="Cash on Delivery" src={cod} alt="Cash on Delivery" />
+                                <Image className="h-10 w-fit bg-white rounded-md p-1" src={easypaisa} alt="Easypaisa" title="Easypaisa" />
+                                <Image className="h-10 w-fit bg-white rounded-md p-1" src={jazzcash} alt="Jazzcash" title="JazzCash" />
+                                <Image className="h-10 w-fit bg-white rounded-md p-1" src={card} alt="Debit/Credit Card" title="Debit/Credit Card" />
+                            </div>
 
-                    </div>
-                    <div className="grid gap-2">
-                        <span className="font-bold text-white">Get App from:</span>
-                        <div className="flex flex-col gap-1">
-                            <Image src={playstore} className="w-36 cursor-pointer" alt="Get it from PlayStore" />
-                            <Image src={appstore} className="w-36 cursor-pointer" alt="Get it from AppStore" />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <span className="font-bold text-white">Get App from:</span>
+                            <div className="flex flex-col gap-1">
+                                <Image src={playstore} className="w-36 cursor-pointer" alt="Get it from PlayStore" />
+                                <Image src={appstore} className="w-36 cursor-pointer" alt="Get it from AppStore" />
+                            </div>
                         </div>
                     </div>
                 </div>
