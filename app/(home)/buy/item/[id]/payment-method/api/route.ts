6ac09c6b5789b79b4bf595168be0e10e018ff40b
agha-naveed <naveedabs31@gmail.com>
2025-04-03 09:14:23 +0500

    ...

--- app/(home)/buy/item/[id]/payment-method/api/route.ts
+++ app/(home)/buy/item/[id]/payment-method/api/route.ts
@@ -45,7 +45,7 @@ export async function POST(req:NextRequest) {
                 _id: buyData.productId
             }, {
                 $inc: {
-                    stock: buyData.quantity
+                    stock: -buyData.quantity
                 }
             })
             if(upd_p) {
