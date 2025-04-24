05ed2feaf5c694f34ba22ca5e30903a67f8773de
agha-naveed <naveedabs31@gmail.com>
2025-04-24 14:46:50 +0500

    .

--- app/(home)/setting/message/page.tsx
+++ app/(home)/setting/message/page.tsx
@@ -32,6 +32,7 @@ interface ConfirmDetail {
 }
 export default function Page() {
     const [message, setMessage] = useState<APIResponse[]>()
+    const [pMessage, setPMessage] = useState<APIItems[]>()
     const [account, setAccount] = useState()
     useEffect(() => {
         async function getData() {
@@ -40,8 +41,13 @@ export default function Page() {
                 const restr = await res.data
                 setAccount(restr.account)
                 if(restr.isExist.email.length > 0) {
-                    console.log(await res.data)
-                    setMessage(await res.data.totalMessages)
+                    if(await restr.account == 'business') {
+                        console.log(await res.data)
+                        setMessage(await res.data.totalMessages)
+                    }
+                    else {
+                        setPMessage(await res.data.totalMessages)
+                    }
                 }
             } catch(err) {
                 alert("You are not logged in!")
@@ -65,74 +71,27 @@ export default function Page() {
         <div>
             {
                 account == "personal" ?
-                message.map((i, idx) => (
+                pMessage.map((i, idx) => (
                     <div className="p-5 flex gap-5 border-b border-b-lightGray hover:bg-lightGray rounded-lg  transition-all cursor-pointer group" key={`seller-notification-${idx}`}>
                         <div className="!w-[120px] h-[120px] border-b border-b-lightGray rounded-lg outline outline-1 overflow-hidden">
-                            {/* <Image src={i?.items[0].imgURL} className="w-full h-full object-cover" alt="" width={200} height={200}/> */}
+                            <Image src={i?.imgURL} className="w-full h-full object-cover" alt="" width={200} height={200}/>
                         </div>
-                        <div className="lg:w-[90%] w-[300px]">
-                            <h3 className="font-semibold group-hover:text-orangeClr line-clamp-2 mb-2">
-                                {/* {i?.items[0].productName} */}
-                            </h3>
-                            <p>
-                                Price: <b>{(i?.items[0].productPrice).toLocaleString()} Pkr</b>
-                            </p>
-                            <p>
-                                Quantity: <b>{i?.items[0].quantity}</b>
-                            </p>
-
-                            <div className="py-2 hidden group-hover:flex justify-between">
-                                <div>
-                                    <div className="flex flex-col">
-                                        <span>
-                                            Recipient{"\'"}s Name: <b>
-                                                {i?.items[0].deliveryAddress.recipientName}
-                                            </b>
-                                        </span>
-                                        <span>
-                                            Cell Number:
-                                            <b>
-                                                {i?.items[0].deliveryAddress.phone_number}
-                                            </b>
-                                        </span>
-                                    </div>
-                                    <div className="flex flex-col">
-                                        <span>
-                                            District: <b>
-                                                {i?.items[0].deliveryAddress.district}
-                                            </b>
-                                        </span>
-                                        <span>
-                                            Address:
-                                            <b>
-                                                {i?.items[0].deliveryAddress.address}
-                                            </b>
-                                        </span>
-                                    </div>
-                                </div>
-
-                                <div className="flex gap-2 self-end">
-                                    <button className="flex transition-all items-center gap-2 bg-orangeClr hover:bg-orange-500 text-white py-2 px-4 rounded-md h-fit" onClick={() => {
-                                        orderStatus({
-                                            pId: i?.items[0].productId,
-                                            cId: i?.userId,
-                                            status: "cancel"
-                                        })
-                                    }}>
-                                        <MdCancelScheduleSend className="text-[18px]" />
-                                        <span>Cancel</span>
-                                    </button>
-                                    <button className="flex transition-all items-center gap-2 bg-slate-800 hover:bg-slate-900 text-white py-2 px-4 rounded-md h-fit" onClick={() => {
-                                        orderStatus({
-                                            pId: i?.items[0].productId,
-                                            cId: i?.userId,
-                                            status: "confirmed"
-                                        })
-                                    }}>
-                                        <FaArrowRight />
-                                        <span>Continue</span>
-                                    </button>
-                                </div>
+                        <div className="flex flex-col">
+                            <div className="font-semibold lg:w-[90%] w-[300px] group-hover:text-orangeClr mb-2">
+                                    Your Order is Confirmed...
+                                    {
+                                        i?.paymentMethod == 'cod' ?
+                                        <p>
+                                            
+                                            Please keep {(i?.productPrice).toLocaleString()} Pkr Ready when Parcel reach at your doorstep.
+                                        .</p>
+                                        : ""
+                                    }
+                            </div>
+                            <div className="lg:w-[90%] w-[300px]">
+                                <h3 className="line-clamp-2 mb-2">
+                                    {i?.productName}
+                                </h3>
                             </div>
                         </div>
                     </div>
