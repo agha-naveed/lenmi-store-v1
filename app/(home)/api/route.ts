9931b58f3cf634e47bd26dc7ce10aaffb6b32380
Syed Naveed Abbas <162593072+agha-naveed@users.noreply.github.com>
2026-06-09 10:13:06 +0500

    Update route.ts

--- app/(home)/api/route.ts
+++ app/(home)/api/route.ts
@@ -5,8 +5,8 @@ import { NextResponse } from "next/server";
 export async function GET() {
     await dbConnection()
 
-    const response = await Product.find().limit(3)
-    const trending = await Product.find().limit(2)
+    const response = await Product.find().lean().limit(3)
+    const trending = await Product.find().lean().limit(2)
     
     
     if(response && trending){
